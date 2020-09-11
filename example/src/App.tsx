c6168d228354f00664e8035b9464dcc10ed0bbdb
Teja <teja.chundru@skillsoft.com>
2020-09-12 01:30:59 +0530

    basic setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import InteractiveTranscripts from 'react-native-interactive-transcripts';
+import Video from 'react-native-video';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    InteractiveTranscripts.multiply(3, 7).then(setResult);
-  }, []);
-
+  let [duration, updateDuration] = useState(0);
+  const progressCallback = (data: any) => {
+    updateDuration(data.currentTime * 1000);
+  };
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Video
+        style={{ width: '100%', height: '30%' }}
+        source={{
+          uri:
+            'https://thepaciellogroup.github.io/AT-browser-tests/video/ElephantsDream.mp4',
+        }} // Can be a URL or a local file.
+        onProgress={progressCallback}
+      />
+      <InteractiveTranscripts
+        currentDuration={duration}
+        url={
+          'https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt'
+        }
+      />
     </View>
   );
 }
@@ -21,5 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100,
   },
 });
